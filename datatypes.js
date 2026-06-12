9bc97875604ac4a9c684d0cbdc2cd9948304cbb0
Dhakshana30 <dhakshana3014@gmail.com>
2026-06-12 15:41:16 +0530

    function.js

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+console.log("Welcome to datatypes")
+
+
+//Numbers
+
+console.log("DT numbers")
+var age=30
+console.log(age)
+
+//string
+console.log(DT string)
+var name="dhaks"
+console.log(age)
+
+//boolean
+console.log(DT boolean)
+var is student="true"
+console.log(is student)
+
+//null
+console.log(DT null)
+var emptyvalue="null"
+console.log(emptyvalue)
+
+//undefined
+console.log(age)
+
