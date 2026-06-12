1e718efc8fdda48881902ac167266f5328035055
Dhakshana30 <dhakshana3014@gmail.com>
2026-06-12 13:59:05 +0530

    logical.js

---
+++ logical.js
@@ -0,0 +1,9 @@
+console.log("logical operators")
+var a=30
+var b=10
+console.log(a > 0 && b > 0)
+console.log(a > 0 && b < 0)
+console.log(a < 0 || b < 0)
+console.log(a < 0 || b > 0)
+console.log(!(a < 0))
+console.log(!(b < 0))
